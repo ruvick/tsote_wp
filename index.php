2c8d91800d4bdb72682346e4031732f75f851119
Ruvick <rudikov.web@ya.ru>
2021-08-04 15:57:16 +0300

    Удалил лишние файлы

--- index.php
+++ index.php
@@ -75,386 +75,53 @@
 				<div class="block__items">
 
 					<div class="services__row block__item tab__item active d-flex">
-						<div class="card__body d-flex">
-							<div class="card__img">
-								<img src="<?php echo get_template_directory_uri(); ?>/img/services/01.jpg" alt="">
-							</div>
-							<div class="card__desc">
-								<h4>
-									Специальная оценка
-									условий труда (СОУТ)
-								</h4>
-								<a href="#" class="card__link">ПОДРОБНЕЕ</a>
-							</div>
-						</div>
+						<?php
+						$posts = get_posts(array(
+							'numberposts' => -1,
+							'category'    => 3,
+							'orderby'     => 'date',
+							'order'       => 'DESC',
+							'include'     => array(),
+							'exclude'     => array(),
+							'meta_key'    => '',
+							'meta_value'  => '',
+							'post_type'   => 'post',
+							'suppress_filters' => true,
+						));
 
-						<div class="card__body d-flex">
-							<div class="card__img">
-								<img src="<?php echo get_template_directory_uri(); ?>/img/services/02.jpg" alt="">
-							</div>
-							<div class="card__desc">
-								<h4>
-									Оценка
-									профессиональных
-									рисков
-								</h4>
-								<a href="#" class="card__link">ПОДРОБНЕЕ</a>
-							</div>
-						</div>
+						$result = wp_get_recent_posts($args);
 
-						<div class="card__body d-flex">
-							<div class="card__img">
-								<img src="<?php echo get_template_directory_uri(); ?>/img/services/03.jpg" alt="">
-							</div>
-							<div class="card__desc">
-								<h4>
-									Проект предельно
-									допустимых выбросов
-									(ПДВ)
-								</h4>
-								<a href="#" class="card__link">ПОДРОБНЕЕ</a>
-							</div>
-						</div>
-
-						<div class="card__body d-flex">
-							<div class="card__img">
-								<img src="<?php echo get_template_directory_uri(); ?>/img/services/04.jpg" alt="">
-							</div>
-							<div class="card__desc">
-								<h4>
-									Декларация о
-									воздействии на
-									окружающую среду
-								</h4>
-								<a href="#" class="card__link">ПОДРОБНЕЕ</a>
-							</div>
-						</div>
-
-						<div class="card__body d-flex">
-							<div class="card__img">
-								<img src="<?php echo get_template_directory_uri(); ?>/img/services/05.jpg" alt="">
-							</div>
-							<div class="card__desc">
-								<h4>
-									Расчет нормативов
-									допустимых выбросов
-								</h4>
-								<a href="#" class="card__link">ПОДРОБНЕЕ</a>
-							</div>
-						</div>
-
-						<div class="card__body d-flex">
-							<div class="card__img">
-								<img src="<?php echo get_template_directory_uri(); ?>/img/services/06.jpg" alt="">
-							</div>
-							<div class="card__desc">
-								<h4>
-									Инвентаризация
-									источников выбросов
-								</h4>
-								<a href="#" class="card__link">ПОДРОБНЕЕ</a>
-							</div>
-						</div>
-
-						<div class="card__body d-flex">
-							<div class="card__img">
-								<img src="<?php echo get_template_directory_uri(); ?>/img/services/07.jpg" alt="">
-							</div>
-							<div class="card__desc">
-								<h4>
-									Проект мероприятий
-									по уменьшению
-									выбросов при НМУ
-								</h4>
-								<a href="#" class="card__link">ПОДРОБНЕЕ</a>
-							</div>
-						</div>
-
-						<div class="card__body d-flex">
-							<div class="card__img">
-								<img src="<?php echo get_template_directory_uri(); ?>/img/services/08.jpg" alt="">
-							</div>
-							<div class="card__desc">
-								<h4>
-									Проект нормативов
-									образования отходов
-									и лимитов на их
-									размещение (ПНООЛР)
-								</h4>
-								<a href="#" class="card__link">ПОДРОБНЕЕ</a>
-							</div>
-						</div>
-
-						<div class="card__body d-flex">
-							<div class="card__img">
-								<img src="<?php echo get_template_directory_uri(); ?>/img/services/09.jpg" alt="">
-							</div>
-							<div class="card__desc">
-								<h4>
-									Инвентаризация
-									отходов производства
-									и потребления
-								</h4>
-								<a href="#" class="card__link">ПОДРОБНЕЕ</a>
-							</div>
-						</div>
-
-						<div class="card__body d-flex">
-							<div class="card__img">
-								<img src="<?php echo get_template_directory_uri(); ?>/img/services/10.jpg" alt="">
-							</div>
-							<div class="card__desc">
-								<h4>
-									Постановка на учет
-									предприятий негативно
-									воздействующих на
-									окружающую среду
-								</h4>
-								<a href="#" class="card__link">ПОДРОБНЕЕ</a>
-							</div>
-						</div>
-
-						<div class="card__body d-flex">
-							<div class="card__img">
-								<img src="<?php echo get_template_directory_uri(); ?>/img/services/11.jpg" alt="">
-							</div>
-							<div class="card__desc">
-								<h4>
-									Разработка проектов
-									санитарно-защитных
-									зон (СЗЗ)
-								</h4>
-								<a href="#" class="card__link">ПОДРОБНЕЕ</a>
-							</div>
-						</div>
-
-						<div class="card__body d-flex">
-							<div class="card__img">
-								<img src="<?php echo get_template_directory_uri(); ?>/img/services/12.jpg" alt="">
-							</div>
-							<div class="card__desc">
-								<h4>
-									Разработка программ
-									производственного
-									экологического
-									контроля (ПЭК)
-								</h4>
-								<a href="#" class="card__link">ПОДРОБНЕЕ</a>
-							</div>
-						</div>
-
-						<div class="card__body d-flex">
-							<div class="card__img">
-								<img src="<?php echo get_template_directory_uri(); ?>/img/services/13.jpg" alt="">
-							</div>
-							<div class="card__desc">
-								<h4>
-									Проведение
-									производственного
-									экологического
-									контроля
-								</h4>
-								<a href="#" class="card__link">ПОДРОБНЕЕ</a>
-							</div>
-						</div>
-
-						<div class="card__body d-flex">
-							<div class="card__img">
-								<img src="<?php echo get_template_directory_uri(); ?>/img/services/14.jpg" alt="">
-							</div>
-							<div class="card__desc">
-								<h4>
-									Проведение
-									инструментальных
-									замеров собственной
-									лабораторией
-								</h4>
-								<a href="#" class="card__link">ПОДРОБНЕЕ</a>
-							</div>
-						</div>
-
-						<div class="card__body d-flex">
-							<div class="card__img">
-								<img src="<?php echo get_template_directory_uri(); ?>/img/services/01.jpg" alt="">
-							</div>
-							<div class="card__desc">
-								<h4>Специальная оценка
-									условий труда (СОУТ)
-								</h4>
-								<a href="#" class="card__link">ПОДРОБНЕЕ</a>
-							</div>
-						</div>
+						foreach ($posts as $post) {
+							get_template_part('template-parts/product-elem');
+						?>
+						<?php
+						}
+						?>
 					</div>
 
 					<div class="services__row block__item tab__item d-flex">
-						<div class="card__body d-flex">
-							<div class="card__img">
-								<img src="<?php echo get_template_directory_uri(); ?>/img/services/04.jpg" alt="">
-							</div>
-							<div class="card__desc">
-								<h4>
-									Декларация о
-									воздействии на
-									окружающую среду
-								</h4>
-								<a href="#" class="card__link">ПОДРОБНЕЕ</a>
-							</div>
-						</div>
+						<?php
+						$posts = get_posts(array(
+							'numberposts' => -1,
+							'category'    => 4,
+							'orderby'     => 'date',
+							'order'       => 'DESC',
+							'include'     => array(),
+							'exclude'     => array(),
+							'meta_key'    => '',
+							'meta_value'  => '',
+							'post_type'   => 'post',
+							'suppress_filters' => true,
+						));
 
-						<div class="card__body d-flex">
-							<div class="card__img">
-								<img src="<?php echo get_template_directory_uri(); ?>/img/services/05.jpg" alt="">
-							</div>
-							<div class="card__desc">
-								<h4>
-									Расчет нормативов
-									допустимых выбросов
-								</h4>
-								<a href="#" class="card__link">ПОДРОБНЕЕ</a>
-							</div>
-						</div>
+						$result = wp_get_recent_posts($args);
 
-						<div class="card__body d-flex">
-							<div class="card__img">
-								<img src="<?php echo get_template_directory_uri(); ?>/img/services/06.jpg" alt="">
-							</div>
-							<div class="card__desc">
-								<h4>
-									Инвентаризация
-									источников выбросов
-								</h4>
-								<a href="#" class="card__link">ПОДРОБНЕЕ</a>
-							</div>
-						</div>
-
-						<div class="card__body d-flex">
-							<div class="card__img">
-								<img src="<?php echo get_template_directory_uri(); ?>/img/services/07.jpg" alt="">
-							</div>
-							<div class="card__desc">
-								<h4>
-									Проект мероприятий
-									по уменьшению
-									выбросов при НМУ
-								</h4>
-								<a href="#" class="card__link">ПОДРОБНЕЕ</a>
-							</div>
-						</div>
-
-						<div class="card__body d-flex">
-							<div class="card__img">
-								<img src="<?php echo get_template_directory_uri(); ?>/img/services/08.jpg" alt="">
-							</div>
-							<div class="card__desc">
-								<h4>
-									Проект нормативов
-									образования отходов
-									и лимитов на их
-									размещение (ПНООЛР)
-								</h4>
-								<a href="#" class="card__link">ПОДРОБНЕЕ</a>
-							</div>
-						</div>
-
-						<div class="card__body d-flex">
-							<div class="card__img">
-								<img src="<?php echo get_template_directory_uri(); ?>/img/services/09.jpg" alt="">
-							</div>
-							<div class="card__desc">
-								<h4>
-									Инвентаризация
-									отходов производства
-									и потребления
-								</h4>
-								<a href="#" class="card__link">ПОДРОБНЕЕ</a>
-							</div>
-						</div>
-
-						<div class="card__body d-flex">
-							<div class="card__img">
-								<img src="<?php echo get_template_directory_uri(); ?>/img/services/10.jpg" alt="">
-							</div>
-							<div class="card__desc">
-								<h4>
-									Постановка на учет
-									предприятий негативно
-									воздействующих на
-									окружающую среду
-								</h4>
-								<a href="#" class="card__link">ПОДРОБНЕЕ</a>
-							</div>
-						</div>
-
-						<div class="card__body d-flex">
-							<div class="card__img">
-								<img src="<?php echo get_template_directory_uri(); ?>/img/services/11.jpg" alt="">
-							</div>
-							<div class="card__desc">
-								<h4>
-									Разработка проектов
-									санитарно-защитных
-									зон (СЗЗ)
-								</h4>
-								<a href="#" class="card__link">ПОДРОБНЕЕ</a>
-							</div>
-						</div>
-
-						<div class="card__body d-flex">
-							<div class="card__img">
-								<img src="<?php echo get_template_directory_uri(); ?>/img/services/12.jpg" alt="">
-							</div>
-							<div class="card__desc">
-								<h4>
-									Разработка программ
-									производственного
-									экологического
-									контроля (ПЭК)
-								</h4>
-								<a href="#" class="card__link">ПОДРОБНЕЕ</a>
-							</div>
-						</div>
-
-						<div class="card__body d-flex">
-							<div class="card__img">
-								<img src="<?php echo get_template_directory_uri(); ?>/img/services/13.jpg" alt="">
-							</div>
-							<div class="card__desc">
-								<h4>
-									Проведение
-									производственного
-									экологического
-									контроля
-								</h4>
-								<a href="#" class="card__link">ПОДРОБНЕЕ</a>
-							</div>
-						</div>
-
-						<div class="card__body d-flex">
-							<div class="card__img">
-								<img src="<?php echo get_template_directory_uri(); ?>/img/services/14.jpg" alt="">
-							</div>
-							<div class="card__desc">
-								<h4>
-									Проведение
-									инструментальных
-									замеров собственной
-									лабораторией
-								</h4>
-								<a href="#" class="card__link">ПОДРОБНЕЕ</a>
-							</div>
-						</div>
-
-						<div class="card__body d-flex">
-							<div class="card__img">
-								<img src="<?php echo get_template_directory_uri(); ?>/img/services/01.jpg" alt="">
-							</div>
-							<div class="card__desc">
-								<h4>Специальная оценка
-									условий труда (СОУТ)
-								</h4>
-								<a href="#" class="card__link">ПОДРОБНЕЕ</a>
-							</div>
-						</div>
+						foreach ($posts as $post) {
+							get_template_part('template-parts/product-elem');
+						?>
+						<?php
+						}
+						?>
 					</div>
 
 				</div>
