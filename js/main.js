1d9e9dfd24f5b286bfebc85042f8058f6911103c
Ruvick <rudikov.web@ya.ru>
2021-08-04 11:34:20 +0300

    Добавил скрипт табов

--- js/main.js
+++ js/main.js
@@ -328,6 +328,23 @@ $('.clients__slider').slick({
 });
 
 
+// Табы
+$('body').on('click', '.tab__navitem', function (event) {
+	var eq = $(this).index();
+	if ($(this).hasClass('parent')) {
+		var eq = $(this).parent().index();
+	}
+	if (!$(this).hasClass('active')) {
+		$(this).closest('.tabs').find('.tab__navitem').removeClass('active');
+		$(this).addClass('active');
+		$(this).closest('.tabs').find('.tab__item').removeClass('active').eq(eq).addClass('active');
+		if ($(this).closest('.tabs').find('.slick-slider').length > 0) {
+			$(this).closest('.tabs').find('.slick-slider').slick('setPosition');
+		}
+	}
+});
+
+
 // Открытие модального окна
 $(".popup-quest").on('click', function (e) {
 	e.preventDefault();
