8407ed2e003969d927077da28756a63234877bef
Ruvick <rudikov.web@ya.ru>
2021-08-04 13:00:20 +0300

    Стр.Контакты

--- page-contacts.php
+++ page-contacts.php
@@ -12,15 +12,12 @@ get_header(); ?>
 <main class="page">
 	<section class="content category">
 		<div class="container">
-
 			<?php
 			if (function_exists('yoast_breadcrumb')) {
 				yoast_breadcrumb('<p id="breadcrumbs">', '</p>');
 			}
 			?>
-
 			<h1><? the_title(); ?></h1>
-
 			<ul>
 				<? $org = carbon_get_theme_option("as_company");
 				if (!empty($org)) { ?><li>Организация: <strong><? echo $org; ?></strong></li><? } ?>
