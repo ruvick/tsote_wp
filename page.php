fa9ced127b0793d0b5ed9e59d8c965c8a32ac3c5
Ruvick <rudikov.web@ya.ru>
2022-05-26 11:49:03 +0300

    Правки

--- page.php
+++ page.php
@@ -29,7 +29,7 @@ get_header(); ?>
 			?> 
 
 			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-				<h1><?php the_title();?></h1>
+				<h1 class="typical-page-title"><?php the_title();?></h1>
 					<?php the_content();?>
 					<?php endwhile;?>
 				<?php endif; ?> 
